1102e36d6a362351c903dbad3f1f65ee397674b6
Edrea Robinson <edrea1979@gmail.com>
2021-10-14 12:08:04 -0500

    Solution

--- findTheRange.js
+++ findTheRange.js
@@ -2,3 +2,17 @@
 let array = [28, 43, -12, 30, 4, 0, -36]
 
 // Write your solution below:
+
+let small = array[0]
+let large = array[0]
+
+for (let i=0; i < array.length; i++){
+    if(array[i] >= large){
+      large = array[i]   
+    }
+    if(array[i] <= small){
+      small = array[i]
+    }
+}
+console.log (small)
+console.log (large)
